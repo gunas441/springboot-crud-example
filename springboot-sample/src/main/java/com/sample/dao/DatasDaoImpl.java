1e786d5f61ba2c353a83bbf76e69a50befa75560
Guna <64432253+gunas441@users.noreply.github.com>
2024-05-09 12:02:43 +0530

    Update DatasDaoImpl.java

--- springboot-sample/src/main/java/com/sample/dao/DatasDaoImpl.java
+++ springboot-sample/src/main/java/com/sample/dao/DatasDaoImpl.java
@@ -3,7 +3,6 @@ package com.sample.dao;
 import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
-
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
